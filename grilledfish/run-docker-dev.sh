647c3e428013c5ed49592e64165ea870303b18d7
Hu,Xiaoming <huxiaoming@live.com>
2020-07-24 09:27:55 +0800

    remove the uwsgi cache, because it does not imporve the performance.update scripts

--- grilledfish/run-docker-dev.sh
+++ grilledfish/run-docker-dev.sh
@@ -1,1 +1,7 @@
-docker run -p 443:443 -v $PWD/grilledfish:/grilledfish/grilledfish -v $PWD/docker/nginx/nginx.conf:/etc/nginx/nginx.conf -v $PWD/docker/nginx/default:/etc/nginx/sites-available/default grilledfish
+docker run -it --network host -p 443:443 \
+    -v $PWD/grilledfish:/grilledfish/grilledfish \
+    -v $PWD/docker/nginx/nginx.conf:/etc/nginx/nginx.conf \
+    -v $PWD/docker/nginx/default:/etc/nginx/sites-available/default \
+    -v $PWD/../Simulator/installer/share/conf:/ipmi_simulator/share/conf \
+    -v $PWD/../Simulator/installer/share/simulator.jar:/ipmi_simulator/share/simulator.jar \
+    grilledfish
